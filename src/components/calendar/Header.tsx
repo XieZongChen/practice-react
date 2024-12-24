b23df972ec759d2ad735ddd282f390cb74838511
XieZongChen <952937570@qq.com>
2024-12-24 10:52:57 +0800

    feat(calendar): header 接入 locale

--- src/components/calendar/Header.tsx
+++ src/components/calendar/Header.tsx
@@ -1,11 +1,18 @@
 import { Dayjs } from 'dayjs';
+import { useContext } from 'react';
+import LocaleContext from './LocaleContext';
+import allLocales from './locale';
+
 interface HeaderProps {
   curMonth: Dayjs;
   prevMonthHandler: () => void;
   nextMonthHandler: () => void;
   todayHandler: () => void;
 }
+
 function Header(props: HeaderProps) {
+  const localeContext = useContext(LocaleContext);
+  const CalendarContext = allLocales[localeContext.locale];
   const { curMonth, prevMonthHandler, nextMonthHandler, todayHandler } = props;
 
   return (
@@ -15,13 +22,13 @@ function Header(props: HeaderProps) {
           &lt;
         </div>
         <div className='calendar-header-value'>
-          {curMonth.format('YYYY 年 MM 月')}
+          {curMonth.format(CalendarContext.formatMonth)}
         </div>
         <div className='calendar-header-icon' onClick={nextMonthHandler}>
           &gt;
         </div>
         <button className='calendar-header-btn' onClick={todayHandler}>
-          今天
+          {CalendarContext.today}
         </button>
       </div>
     </div>
