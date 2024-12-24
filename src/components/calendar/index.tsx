5b3e59172e6dc6b4a076f1c54b8084371fea0192
XieZongChen <952937570@qq.com>
2024-12-24 10:07:07 +0800

    feat(calendar): 添加 classname & style api

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,15 +1,29 @@
+import { CSSProperties, ReactNode } from 'react';
 import { Dayjs } from 'dayjs';
+import cs from 'classnames';
 import MonthCalendar from './MonthCalendar';
 import './index.scss';
 import Header from './Header';
 
 export interface CalendarProps {
   value: Dayjs;
+  style?: CSSProperties;
+  className?: string | string[];
+  // 定制日期显示，会完全覆盖日期单元格
+  dateRender?: (currentDate: Dayjs) => ReactNode;
+  // 定制日期单元格，内容会被添加到单元格内，只在全屏日历模式下生效。
+  dateInnerContent?: (currentDate: Dayjs) => ReactNode;
+  // 国际化相关
+  locale?: string;
+  onChange?: (date: Dayjs) => void;
 }
 
 function Calendar(props: CalendarProps) {
+  const { className, style } = props;
+  const mergedClassNames = cs('calendar', className);
+  
   return (
-    <div className='calendar'>
+    <div className={mergedClassNames} style={style}>
       <Header />
       <MonthCalendar {...props} />
     </div>
