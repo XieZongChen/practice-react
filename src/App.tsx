b5e89de1ca6e94163a9b72dcf0b2c7f60da38751
XieZongChen <952937570@qq.com>
2024-12-26 10:17:36 +0800

    feat(icon): 开发 createIcon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Calendar from '@/components/calendar';
-import dayjs from 'dayjs';
+import { IconAdd } from './components/icon/icons/IconAdd';
+import { IconEmail } from './components/icon/icons/IconEmail';
 
 function App() {
   return (
-    <Calendar
-      defaultValue={dayjs('2024-7-11')}
-    ></Calendar>
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <IconAdd size='40px'></IconAdd>
+      <IconAdd spin></IconAdd>
+      <IconEmail style={{ color: 'blue', fontSize: '50px' }}></IconEmail>
+    </div>
   );
 }
 
