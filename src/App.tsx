f4f4a9d1dab4679cb83dc2b16f0c4101c4cc3de8
XieZongChen <952937570@qq.com>
2024-12-26 10:25:17 +0800

    feat(icon): 开发 createFromIconfont

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { createFromIconfont } from './components/icon/createFrontIconfont';
 import { IconAdd } from './components/icon/icons/IconAdd';
 import { IconEmail } from './components/icon/icons/IconEmail';
+
+const IconFont = createFromIconfont(
+  '//at.alicdn.com/t/c/font_4443338_a2wwqhorbk4.js'
+);
 
 function App() {
   return (
@@ -7,6 +12,12 @@ function App() {
       <IconAdd size='40px'></IconAdd>
       <IconAdd spin></IconAdd>
       <IconEmail style={{ color: 'blue', fontSize: '50px' }}></IconEmail>
+      <IconFont type='icon-shouye-zhihui' size='40px'></IconFont>
+      <IconFont
+        type='icon-gerenzhongxin-zhihui'
+        fill='blue'
+        size='40px'
+      ></IconFont>
     </div>
   );
 }
---
+++ src/components/icon/createFrontIconfont.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Icon, IconProps } from './';
+
+const loadedSet = new Set<string>();
+
+export function createFromIconfont(scriptUrl: string) {
+  if (
+    typeof scriptUrl === 'string' &&
+    scriptUrl.length &&
+    // 防止加载过的再次加载
+    !loadedSet.has(scriptUrl)
+  ) {
+    // 在 document.body 上添加 <script> 标签引入传入地址
+    const script = document.createElement('script');
+    script.setAttribute('src', scriptUrl);
+    script.setAttribute('data-namespace', scriptUrl);
+    document.body.appendChild(script);
+
+    loadedSet.add(scriptUrl);
+  }
+
+  const Iconfont = React.forwardRef<SVGSVGElement, IconProps>((props, ref) => {
+    const { type, ...rest } = props;
+
+    return (
+      <Icon {...rest} ref={ref}>
+        {type ? (
+          // 使用 use 引用
+          <use xlinkHref={`#${type}`} />
+        ) : null}
+      </Icon>
+    );
+  });
+
+  return Iconfont;
+}
