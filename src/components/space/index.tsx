eaad2225a0ad16b3f06a595af7dfe4c84c4a2fab
XieZongChen <952937570@qq.com>
2024-12-27 10:04:41 +0800

    feat(space): 规划 api

--- src/components/space/index.tsx
+++ src/components/space/index.tsx
@@ -1,6 +1,34 @@
+export type SizeType = 'small' | 'middle' | 'large' | number | undefined;
+
 export interface SpaceProps extends React.HTMLAttributes<HTMLDivElement> {
+  /**
+   * 类名
+   */
   className?: string;
+  /**
+   * 样式
+   */
   style?: React.CSSProperties;
+  /**
+   * 间距
+   */
+  size?: SizeType | [SizeType, SizeType];
+  /**
+   * 排布方式
+   */
+  direction?: 'horizontal' | 'vertical';
+  /**
+   * 对齐方式
+   */
+  align?: 'start' | 'end' | 'center' | 'baseline';
+  /**
+   * 分隔符
+   */
+  split?: React.ReactNode;
+  /**
+   * 是否可换行
+   */
+  wrap?: boolean;
 }
 
 const Space: React.FC<SpaceProps> = (props) => {
