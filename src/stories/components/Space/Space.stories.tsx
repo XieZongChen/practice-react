f027fcefb5842355e3fb7f588d16a8f03947d01e
XieZongChen <952937570@qq.com>
2024-12-27 11:08:52 +0800

    docs(space): 增加文档

---
+++ src/stories/components/Space/Space.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Space, { SpaceProps } from '@/components/space';
+
+const demoBoxStyle = {
+  width: '100px',
+  height: '100px',
+  background: 'pink',
+  border: '1px solid #000',
+};
+
+const renderSpace = (args: SpaceProps) => {
+  return (
+    <Space {...args}>
+      <div style={demoBoxStyle}></div>
+      <div style={demoBoxStyle}></div>
+      <div style={demoBoxStyle}></div>
+    </Space>
+  );
+};
+
+const meta = {
+  title: 'components/Space',
+  component: Space,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof Space>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Size: Story = {
+  args: {
+    size: [20, 30],
+  },
+  render: renderSpace,
+};
+
+export const Direction: Story = {
+  args: {
+    direction: 'vertical',
+  },
+  render: renderSpace,
+};
+
+export const Align: Story = {
+  args: {
+    align: 'end',
+  },
+  render: renderSpace,
+};
+
+export const Split: Story = {
+  args: {
+    split: '|',
+  },
+  render: renderSpace,
+};
