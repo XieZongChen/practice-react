78c7a0bdd2b8c07776fe874b02a6372fbece6456
XieZongChen <952937570@qq.com>
2025-01-10 09:57:38 +0800

    feat(popover): 开发组件逻辑

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -5,6 +5,17 @@ import {
   useRef,
   useState,
 } from 'react';
+import {
+  useInteractions,
+  useFloating,
+  useClick,
+  useDismiss,
+  offset,
+  arrow,
+  FloatingArrow,
+  flip,
+  useHover,
+} from '@floating-ui/react';
 
 type Alignment = 'start' | 'end';
 type Side = 'top' | 'right' | 'bottom' | 'left';
@@ -32,5 +43,63 @@ export default function Popover(props: PopoverProps) {
     style,
   } = props;
 
-  return <></>;
+  const arrowRef = useRef(null);
+
+  const [isOpen, setIsOpen] = useState(open);
+
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: (open) => {
+      setIsOpen(open);
+      onOpenChange?.(open);
+    },
+    placement,
+    middleware: [
+      offset(10),
+      arrow({
+        element: arrowRef,
+      }),
+      flip(),
+    ],
+  });
+
+  const interaction =
+    trigger === 'hover' ? useHover(context) : useClick(context);
+
+  const dismiss = useDismiss(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    interaction,
+    dismiss,
+  ]);
+
+  return (
+    <>
+      <span
+        ref={refs.setReference}
+        {...getReferenceProps()}
+        className={className}
+        style={style}
+      >
+        {children}
+      </span>
+      {isOpen && (
+        <div
+          className='popover-floating'
+          ref={refs.setFloating}
+          style={floatingStyles}
+          {...getFloatingProps()}
+        >
+          {content}
+          <FloatingArrow
+            ref={arrowRef}
+            context={context}
+            fill='#fff'
+            stroke='#000'
+            strokeWidth={1}
+          />
+        </div>
+      )}
+    </>
+  );
 }
