a56673371352f0fedb5462fd367d405c68652cee
XieZongChen <952937570@qq.com>
2025-01-10 10:25:36 +0800

    feat(popover): 优化浮层渲染位置

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -2,9 +2,11 @@ import {
   CSSProperties,
   PropsWithChildren,
   ReactNode,
+  useMemo,
   useRef,
   useState,
 } from 'react';
+import { createPortal } from 'react-dom';
 import {
   useInteractions,
   useFloating,
@@ -73,6 +75,35 @@ export default function Popover(props: PopoverProps) {
     dismiss,
   ]);
 
+  // 浮层渲染包装
+  // todo: 后续可以增加 props 规定渲染位置
+  const el = useMemo(() => {
+    const el = document.createElement('div');
+    el.className = `wrapper`;
+
+    document.body.appendChild(el);
+    return el;
+  }, []);
+
+  // 浮层节点
+  const floating = isOpen && (
+    <div
+      className='popover-floating'
+      ref={refs.setFloating}
+      style={floatingStyles}
+      {...getFloatingProps()}
+    >
+      {content}
+      <FloatingArrow
+        ref={arrowRef}
+        context={context}
+        fill='#fff'
+        stroke='#000'
+        strokeWidth={1}
+      />
+    </div>
+  );
+
   return (
     <>
       <span
@@ -83,23 +114,8 @@ export default function Popover(props: PopoverProps) {
       >
         {children}
       </span>
-      {isOpen && (
-        <div
-          className='popover-floating'
-          ref={refs.setFloating}
-          style={floatingStyles}
-          {...getFloatingProps()}
-        >
-          {content}
-          <FloatingArrow
-            ref={arrowRef}
-            context={context}
-            fill='#fff'
-            stroke='#000'
-            strokeWidth={1}
-          />
-        </div>
-      )}
+      {/* 将浮层渲染到指定 dom 上，避免外部样式影响 */}
+      {createPortal(floating, el)}
     </>
   );
 }
