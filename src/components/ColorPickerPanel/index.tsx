47e28e0f88957d2869be0d8dacedafa87ee7a480
XieZongChen <952937570@qq.com>
2025-01-13 09:58:47 +0800

    feat(ColorPickerPanel): 组件规划

---
+++ src/components/ColorPickerPanel/index.tsx
@@ -0,0 +1,20 @@
+import { CSSProperties } from 'react';
+import cs from 'classnames';
+import './index.scss';
+
+export interface ColorPickerProps {
+  className?: string;
+  style?: CSSProperties;
+  value?: string;
+  onChange?: (color: string) => void;
+}
+
+function ColorPickerPanel(props: ColorPickerProps) {
+  const { className, style } = props;
+
+  const classNames = cs('color-picker', className);
+
+  return <div className={classNames} style={style}></div>;
+}
+
+export default ColorPickerPanel;
