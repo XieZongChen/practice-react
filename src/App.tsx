65207e26d1bb5fd1cc93306520df6e8f1be8ed01
XieZongChen <952937570@qq.com>
2025-01-15 10:47:31 +0800

    feat(Tour): 测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
 import React, { useRef } from 'react';
 import ColorPickerPanel from './components/ColorPickerPanel';
+import { Mask } from './components/Tour/Mask';
+import Popover from './components/Popover';
 
 function App() {
-  return <ColorPickerPanel defaultValue="rgb(44 53 156)"></ColorPickerPanel>;
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Mask
+        element={document.getElementById('test')!}
+        renderMaskContent={(wrap) => (
+          <Popover
+            content={
+              <div style={{ width: 300 }}>
+                <p>hello</p>
+                <button>下一步</button>
+              </div>
+            }
+            open={true}
+          >
+            {wrap}
+          </Popover>
+        )}
+      ></Mask>
+      <div id='test' style={{ width: 'max-content' }}>
+        test!
+      </div>
+    </div>
+  );
 }
 
 export default App;
