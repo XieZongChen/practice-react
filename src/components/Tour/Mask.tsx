54416ebf5f43fd7a1f19088bdf25eb90562234ea
XieZongChen <952937570@qq.com>
2025-01-15 09:55:13 +0800

    feat(Tour): 开发 Mask

---
+++ src/components/Tour/Mask.tsx
@@ -0,0 +1,48 @@
+import React, { CSSProperties, useEffect, useState } from 'react';
+import { getMaskStyle } from './getMaskStyle';
+
+interface MaskProps {
+  element: HTMLElement; // 目标元素
+  container?: HTMLElement; // 遮罩层所在容器
+  renderMaskContent?: (wrapper: React.ReactNode) => React.ReactNode;
+}
+
+export const Mask: React.FC<MaskProps> = (props) => {
+  const { element, renderMaskContent, container } = props;
+
+  const [style, setStyle] = useState<CSSProperties>({});
+
+  useEffect(() => {
+    if (!element) {
+      return;
+    }
+
+    // 确保目标元素在可视区域
+    element.scrollIntoView({
+      block: 'center',
+      inline: 'center',
+    });
+
+    const style = getMaskStyle(element, container || document.documentElement);
+
+    setStyle(style);
+  }, [element, container]);
+
+  const getContent = () => {
+    if (!renderMaskContent) {
+      return null;
+    }
+    return renderMaskContent(
+      <div
+        className={'mask-content'}
+        style={{ width: '100%', height: '100%' }}
+      />
+    );
+  };
+
+  return (
+    <div style={style} className='mask'>
+      {getContent()}
+    </div>
+  );
+};
