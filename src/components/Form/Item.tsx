690d0ba43ae14139d6836c286bd17dacd527d340
XieZongChen <952937570@qq.com>
2025-01-17 10:00:00 +0800

    feat(Form): 优化代码文档

--- src/components/Form/Item.tsx
+++ src/components/Form/Item.tsx
@@ -18,11 +18,22 @@ export interface ItemProps {
   style?: CSSProperties;
   label?: ReactNode;
   name?: string;
+  /**
+   * 子节点值的属性
+   * 默认为 value
+   * 当表单项为 checkbox、switch 等时，应该传 checked
+   */
   valuePropName?: string;
   rules?: Array<Record<string, any>>;
+  /**
+   * 这里重新申明 children 的类型，保证有值时一定是个 React 组件
+   */
   children?: ReactElement;
 }
 
+/**
+ * 根据表单项类型来获取 value 值
+ */
 const getValueFromEvent = (e: ChangeEvent<HTMLInputElement>) => {
   const { target } = e;
   if (target.type === 'checkbox') {
@@ -40,10 +51,9 @@ const Item: FC<ItemProps> = (props: ItemProps) => {
 
   const [value, setValue] = useState<string | number | boolean>();
   const [error, setError] = useState('');
-
   const { onValueChange, values, validateRegister } = useContext(FormContext);
-
   useEffect(() => {
+    // 当 context 中本组件的数据发生变更时，同步更新本组件内部的 value
     if (name && value !== values?.[name]) {
       setValue(values?.[name]);
     }
@@ -79,11 +89,13 @@ const Item: FC<ItemProps> = (props: ItemProps) => {
 
   useEffect(() => {
     if (name) {
+      // context 注册 name 对应的 validator 函数
       validateRegister?.(name, () => handleValidate(value));
     }
   }, [value]);
 
   if (!name) {
+    // 如果没 name 参数，说明是提交按钮等只需要样式的情况，直接返回 children
     return children;
   }
 
@@ -94,12 +106,16 @@ const Item: FC<ItemProps> = (props: ItemProps) => {
     propsName.value = value;
   }
 
+  /**
+   * 用 React.cloneElement 的形式为 children 传入 value、onChange 等参数
+   */
   const childEle =
     React.Children.toArray(children).length > 1
       ? children
       : React.cloneElement(children!, {
           ...propsName,
           onChange: (e: ChangeEvent<HTMLInputElement>) => {
+            // onChange 回调里设置 value，并且修改 context 里的 values 的值
             const value = getValueFromEvent(e);
             setValue(value);
             onValueChange?.(name, value);
