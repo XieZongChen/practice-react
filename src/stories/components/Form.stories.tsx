8baa7c185a6fd0e5b73212f2531f95c5db3ce0c8
XieZongChen <952937570@qq.com>
2025-01-19 09:40:40 +0800

    docs(Form): 添加 storybook 文档

---
+++ src/stories/components/Form.stories.tsx
@@ -0,0 +1,89 @@
+import Form from '@/components/Form';
+import { FormProps, FormRefApi } from '@/components/Form/Form';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button, Checkbox, Input } from 'antd';
+import { FC, useRef } from 'react';
+
+const RenderForm: FC = (args: FormProps) => {
+  const form = useRef<FormRefApi>(null);
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+  return (
+    <>
+      <Form
+        ref={form}
+        initialValues={{ remember: true, username: 'test-init' }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label='Username'
+          name='username'
+          rules={[
+            { required: true, message: '请输入用户名!' },
+            { max: 6, message: '长度不能大于 6' },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label='Password'
+          name='password'
+          rules={[{ required: true, message: '请输入密码!' }]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item name='remember' valuePropName='checked'>
+          <Checkbox>记住我</Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <div>
+            <Button type='primary' htmlType='submit'>
+              登录
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+      <Button
+        type='primary'
+        style={{ marginTop: '10px' }}
+        onClick={() => {
+          console.log(form.current?.getFieldsValue());
+        }}
+      >
+        控制台打印表单值
+      </Button>
+    </>
+  );
+};
+
+const meta = {
+  title: 'components/Form',
+  component: RenderForm,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof Form>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const InitialValues: Story = {
+  args: {
+    initialValues: { remember: true, username: 'test-init' },
+  },
+  render: RenderForm as any,
+};
